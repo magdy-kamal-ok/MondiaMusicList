dc8a5379262bc4e4a83540dd4266dbcc7229f801
mac <magdy.kamal.ok@gmail.com>
2019-07-08 03:52:28 +0200

    Initial Commit

---
+++ ModiaMusicList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModiaMusicList
+//
+//  Created by mac on 7/8/19.
+//  Copyright © 2019 OwnProjects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
